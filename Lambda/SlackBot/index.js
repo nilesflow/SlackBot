927b01a5d92898d884ab7aa21b8c95bee9f896e7
nilesflow <nilesflow@gmail.com>
2018-06-03 01:31:25 +0900

    first commit

---
+++ Lambda/SlackBot/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var slack = require('nodefw').slack;
+var apigw = require('nodefw').aws.apigateway;
+
+exports.handler = async (event) => {
+    console.log(event);
+
+    var s = new slack.Bot({
+        channel_id: process.env.SLACK_NOTIFY_CHANNEL_ID,
+        bot_token: process.env.SLACK_BOT_TOKEN,
+        user_token: process.env.SLACK_USER_TOKEN,
+        verify_token: process.env.SLACK_APP_VERIFY_TOKEN,
+    });
+    await s.receiveEvent(event).catch(e => {
+        // エラー応答
+        if (e instanceof slack.error.SlackError) {
+            // 自コードのエラー
+            console.error('receiveEvent: '+ e.stack);
+            // API Gatewayで解釈できる形に変換
+            throw new apigw.ApiGatewayError(e.statusCode, e.name, e.message);
+        }
+        // それ以外のエラー
+        throw new apigw.ApiGatewayError(500, "InternalError", e.message);
+    });
+
+    // 正常終了
+    console.log('success');
+    return;
+};
